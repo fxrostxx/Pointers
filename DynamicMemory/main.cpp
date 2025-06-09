c1b8f4f56bc4e231f7f27f7ac65042c2e5709fcf
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-06-09 21:25:07 +0300

    TODO: Insert, Erase

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 void FillArr(int arr[], const int n);
 void PrintArr(int arr[], const int n);
+int* PushBack(int arr[], int &n, const int value);
+int* PushFront(int arr[], int &n, const int value);
+int* PopBack(int arr[], int& n);
+int* PopFront(int arr[], int& n);
 
 int main()
 {
@@ -16,6 +20,31 @@ int main()
 	FillArr(arr, n);
 	PrintArr(arr, n);
 
+	int value;
+	cout << "Введите значение добавляемого элемента: ";
+	cin >> value;
+
+	arr = PushBack(arr, n, value);
+
+	PrintArr(arr, n);
+
+	cout << "Введите значение добавляемого элемента: ";
+	cin >> value;
+
+	arr = PushFront(arr, n, value);
+
+	PrintArr(arr, n);
+
+	arr = PopBack(arr, n);
+
+	cout << "Удален последний элемент: " << endl;
+	PrintArr(arr, n);
+
+	arr = PopFront(arr, n);
+
+	cout << "Удален первый элемент: " << endl;
+	PrintArr(arr, n);
+
 	delete[] arr;
 
 	return 0;
@@ -23,16 +52,62 @@ int main()
 
 void FillArr(int arr[], const int n)
 {
-	for (int i = 0; i < n; ++i)
-	{
-		*(arr + i) = rand() % 100;
-	}
+	for (int i = 0; i < n; ++i) *(arr + i) = rand() % 100;
 }
 void PrintArr(int arr[], const int n)
 {
-	for (int i = 0; i < n; ++i)
-	{
-		cout << arr[i] << "\t";
-	}
+	for (int i = 0; i < n; ++i) cout << arr[i] << "\t";
 	cout << endl;
+}
+
+int* PushBack(int arr[], int &n, const int value)
+{
+	int* newArr = new int[n + 1];
+
+	for (int i = 0; i < n; ++i) newArr[i] = arr[i];
+
+	delete[] arr;
+
+	newArr[n] = value;
+
+	++n;
+
+	return newArr;
+}
+
+int* PushFront(int arr[], int& n, const int value)
+{
+	int* newArr = new int[n + 1];
+
+	for (int i = 0; i < n; ++i) newArr[i + 1] = arr[i];
+
+	delete[] arr;
+
+	newArr[0] = value;
+
+	++n;
+
+	return newArr;
+}
+
+int* PopBack(int arr[], int& n)
+{
+	int* newArr = new int[--n];
+
+	for (int i = 0; i < n; ++i) newArr[i] = arr[i];
+
+	delete[] arr;
+
+	return newArr;
+}
+
+int* PopFront(int arr[], int& n)
+{
+	int* newArr = new int[--n];
+
+	for (int i = 0; i < n; ++i) newArr[i] = arr[i + 1];
+
+	delete[] arr;
+
+	return newArr;
 }
