5cbe2a55bc075fe1d55ade85ed2eac73c6444a37
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-06-11 21:19:25 +0300

    DynamicArrays2 START

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
-void FillArr(int arr[], const int n);
+void FillArr(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillArr(int** arr, const int rows, const int cols, int minRand = 0, int maxRand = 100);
 void PrintArr(int arr[], const int n);
+void PrintArr(int** arr, const int rows, const int cols);
 int* PushBack(int arr[], int &n, const int value);
 int* PushFront(int arr[], int &n, const int value);
 int* Insert(int arr[], int& n, const int index, const int value);
@@ -10,10 +15,17 @@ int* PopBack(int arr[], int& n);
 int* PopFront(int arr[], int& n);
 int* Erase(int arr[], int& n, const int index);
 
+int** PushRowBack(int** arr, int &rows, int cols);
+void PushColBack(int** arr, const int rows, int& cols);
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: ";
 	cin >> n;
@@ -66,18 +78,85 @@ int main()
 	PrintArr(arr, n);
 
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+
+	int rows, cols;
+
+	cout << "Введите количество строк: ";
+	cin >> rows;
+	cout << "Введите количество столбцов: ";
+	cin >> cols;
+
+	int** arr = new int* [rows];
+
+	for (int i = 0; i < rows; ++i)
+	{
+		arr[i] = new int[cols];
+	}
+
+	FillArr(arr, rows, cols);
+
+	PrintArr(arr, rows, cols);
+
+	cout << "Добавление строки в конец:" << endl;
+
+	arr = PushRowBack(arr, rows, cols);
+
+	FillArr(arr[rows - 1], cols, 100, 1000);
+
+	PrintArr(arr, rows, cols);
+
+	cout << "Добавление столбца в конец:" << endl;
+
+	PushColBack(arr, rows, cols);
+
+	for (int i = 0; i < rows; ++i) arr[i][cols - 1] = rand() % 900 + 100;
+
+	PrintArr(arr, rows, cols);
+
+	for (int i = 0; i < rows; ++i)
+	{
+		delete[] arr[i];
+	}
+
+	delete[] arr;
+
+#endif // DYNAMIC_MEMORY_2
 
 	return 0;
 }
 
-void FillArr(int arr[], const int n)
+void FillArr(int arr[], const int n, int minRand, int maxRand)
 {
-	for (int i = 0; i < n; ++i) *(arr + i) = rand() % 100;
+	for (int i = 0; i < n; ++i) *(arr + i) = rand() % (maxRand - minRand) + minRand;
+}
+void FillArr(int** arr, const int rows, const int cols, int minRand, int maxRand)
+{
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+		}
+	}
 }
 void PrintArr(int arr[], const int n)
 {
 	for (int i = 0; i < n; ++i) cout << arr[i] << "\t";
 	cout << endl;
+}
+void PrintArr(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
 
 int* PushBack(int arr[], int &n, const int value)
@@ -165,4 +244,38 @@ int* Erase(int arr[], int& n, const int index)
 
 		return newArr;
 	}
+}
+
+int** PushRowBack(int** arr, int& rows, int cols)
+{
+	int** newArr = new int* [rows + 1];
+
+	for (int i = 0; i < rows; ++i)
+	{
+		newArr[i] = arr[i];
+	}
+
+	delete[] arr;
+
+	newArr[rows] = new int[cols] {};
+
+	++rows;
+
+	return newArr;
+}
+
+void PushColBack(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; ++i)
+	{
+		int* temp = new int[cols + 1] {};
+
+		for (int j = 0; j < cols; ++j) temp[j] = arr[i][j];
+
+		delete[] arr[i];
+
+		arr[i] = temp;
+	}
+
+	++cols;
 }
